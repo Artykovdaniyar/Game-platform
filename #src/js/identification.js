93c2a36fa9bc74bfc228099cddf807f19a3dcb5a
Daniir Artykov <artykovdaniyar@gmail.com>
2021-08-24 05:49:14 +0600

    Завершение верстки страницы о придложении

--- #src/js/identification.js
+++ #src/js/identification.js
@@ -1,6 +1,23 @@
 const loadPersonalDocImgInput = document.querySelector(".identification__person-doc-input");
 const loadAdressDocImgInput = document.querySelector(".identification__address-doc-input");
+const loadPersonalDocImgBtn = document.querySelector(".identification__person-doc-btn");
+const loadAdressDocImgBtn = document.querySelector(".identification__address-doc-btn");
+
+
 const previewCloseBtn = document.querySelectorAll(".identification__preview-close-btn");
+const allInputs = document.querySelectorAll(".input");
+const submitBtn = document.querySelector(".identification__submit-btn");
+const previewWrappers = document.querySelectorAll(".identification__preview-wrapper");
+
+
+import formValidChecker from "./modules/formValidChecker.js";
+
+
+
+
+allInputs.forEach(input => {
+    formValidChecker(input)
+});
 
 
 
@@ -32,8 +49,28 @@ function previewloadedImg (input) {
 }
 
 
+
+
 previewloadedImg(loadAdressDocImgInput);
 previewloadedImg(loadPersonalDocImgInput);
 
 
 
+submitBtn.addEventListener("click", function(e){
+    allInputs.forEach(input =>{
+        if(input.value == "" || input.classList.contains("invalid__input")){
+            e.preventDefault()
+            input.classList.add("invalid__input")
+            input.addEventListener("focus", function(e){
+                e.target.classList.remove("invalid__input")
+            });
+        }
+    })
+
+    if(loadPersonalDocImgBtn.style.display != "none" || loadAdressDocImgBtn.style.display != "none"){
+        e.preventDefault();
+        loadPersonalDocImgBtn.classList.add("invalid__input");
+        loadAdressDocImgBtn.classList.add("invalid__input");
+    }
+
+});
--- dist/js/mobile-header.js
+++ dist/js/mobile-header.js
@@ -1,5 +1,6 @@
 import goPreviousPage from "./modules/goPreviousPage.js" ;
 import headerMobileAnimetion from "./modules/headerMobileAnimetion.js"
 
+
 goPreviousPage();
 headerMobileAnimetion();
